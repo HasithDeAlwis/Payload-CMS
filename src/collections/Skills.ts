91dd304c53b50439a1708c1fb4d9a9fed8d33384
HasithDeAlwis <hasithde24@gmail.com>
2024-10-22 18:34:40 -0400

    feat(skills): create skills collections
    Includes the currently learning and current skills collections

---
+++ src/collections/Skills.ts
@@ -0,0 +1,85 @@
+import { CollectionConfig } from "payload"
+
+export const LearnedSkill: CollectionConfig = {
+    slug: 'learned-skill-logos',
+    access: {
+      read: () => true,
+      create: () => true,
+      delete: () => true,
+      update: () => true,
+    },
+    fields: [
+      {
+        name: 'skillOrder',
+        type: 'number',
+        required: true,
+        admin: {
+          description: 'What order should this svg appear in?',
+        },
+      },
+      {
+        name: 'skillLogo',
+        type: 'relationship',
+        relationTo: 'media', // References the 'media' collection
+        label: 'Media File',
+        admin: {
+          description: 'Select a svg that represents an trait about you',
+        },
+        required: true,
+      },
+      {
+        name: 'skillHref',
+        type: 'text',
+        required: true,
+        admin: {
+            description: 'Enter a link to the skill'
+        }
+      }
+    ],
+  }
+
+  export const CurrentlyLearningSkill: CollectionConfig = {
+    slug: 'currently-learning-skill',
+    access: {
+      read: () => true,
+      create: () => true,
+      delete: () => true,
+      update: () => true,
+    },
+    fields: [
+      {
+        name: 'currentSkillName',
+        type: 'text',
+        required: true,
+        admin: {
+          description: 'What order should this svg appear in?',
+        },
+      },
+      {
+        name: 'currentSkillLogo',
+        type: 'relationship',
+        relationTo: 'media', // References the 'media' collection
+        label: 'Media File',
+        admin: {
+          description: 'Select a svg that represents an trait about you',
+        },
+        required: true,
+      },
+      {
+        name: 'currentSkillHref',
+        type: 'text',
+        required: true,
+        admin: {
+            description: 'Enter a link to the skill'
+        }
+      },
+      {
+        name: 'currentSkillDescription',
+        type: 'textarea',
+        required: true,
+        admin: {
+            description: 'Enter a description of the skill'
+        }
+      }
+    ],
+  }
