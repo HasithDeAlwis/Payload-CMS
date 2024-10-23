2ad6eba5c4ad2ea4bda91b2307f6b6aec3890776
HasithDeAlwis <hasithde24@gmail.com>
2024-10-23 09:45:09 -0400

    feat: create project collection

---
+++ src/collections/Projects.ts
@@ -0,0 +1,59 @@
+import { CollectionConfig } from "payload"
+
+export const Projects: CollectionConfig = {
+  slug: 'projects',
+  access: {
+    read: () => true,
+    create: () => true,
+    delete: () => true,
+    update: () => true,
+  },
+  labels: {
+    singular: 'Project',
+    plural: 'Projects',
+  },
+  admin: {
+    useAsTitle: 'title', // Use the title as the identifier in the admin panel
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+      required: true,
+    },
+    {
+      name: 'sourceLink',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'demoLink',
+      type: 'text',
+      required: false,
+    },
+    {
+      name: 'readMoreLink',
+      type: 'text',
+      required: false,
+    },
+    {
+        name: 'projectImage',
+        type: 'relationship',
+        relationTo: 'media', // References the 'media' collection
+        label: 'Media File',
+        admin: {
+            description: 'Select a svg that represents an trait about you',
+        },
+    },
+    {
+      name: 'videoLink',
+      type: 'text',
+      required: false,
+    },
+  ],
+};
